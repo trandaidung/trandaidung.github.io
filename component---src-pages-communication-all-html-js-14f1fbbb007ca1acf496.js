c8e33787cb0d6453194c0759e20744699d534f6e
Dung Tran <trandaidung2810@gmail.com>
2018-12-12 21:13:59 +0700

    Updates

---
+++ component---src-pages-communication-all-html-js-14f1fbbb007ca1acf496.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[10],{190:function(n,t,a){"use strict";a.r(t),function(n){a.d(t,"pageQuery",function(){return c});a(0);var o=a(228),c="777782760";t.default=function(t){var a=t.data,c=t.location;return n.createElement(o.a,{blogType:"communication",data:a,location:c})}}.call(this,a(58))}}]);
+//# sourceMappingURL=component---src-pages-communication-all-html-js-14f1fbbb007ca1acf496.js.map
---
+++ component---src-pages-vocabulary-all-html-js-27ed0e79c52ac450f6c3.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[14],{194:function(a,n,t){"use strict";t.r(n),function(a){t.d(n,"pageQuery",function(){return e});t(0);var o=t(228),e="3433664790";n.default=function(n){var t=n.data,e=n.location;return a.createElement(o.a,{blogType:"vocabulary",data:t,location:e})}}.call(this,t(58))}}]);
+//# sourceMappingURL=component---src-pages-vocabulary-all-html-js-27ed0e79c52ac450f6c3.js.map
