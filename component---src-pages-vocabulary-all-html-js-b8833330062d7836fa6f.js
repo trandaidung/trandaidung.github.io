049c7619a1d8865d1deb364896d054f2d609e821
dungtd <trandaidung2810@gmail.com>
2018-12-15 18:39:37 +0700

    Updates

---
+++ component---src-pages-vocabulary-all-html-js-b8833330062d7836fa6f.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[14],{194:function(a,n,t){"use strict";t.r(n),function(a){t.d(n,"pageQuery",function(){return e});t(0);var o=t(230),e="3433664790";n.default=function(n){var t=n.data,e=n.location;return a.createElement(o.a,{blogType:"vocabulary",data:t,location:e})}}.call(this,t(58))}}]);
+//# sourceMappingURL=component---src-pages-vocabulary-all-html-js-b8833330062d7836fa6f.js.map
