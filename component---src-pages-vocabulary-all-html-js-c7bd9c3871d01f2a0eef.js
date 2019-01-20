3f43e1ec01a3dd81933c9f03b149cfc904fb3ad5
dungtd <trandaidung2810@gmail.com>
2019-01-20 16:38:31 +0700

    Updates

---
+++ component---src-pages-vocabulary-all-html-js-c7bd9c3871d01f2a0eef.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[13],{193:function(a,n,t){"use strict";t.r(n),function(a){t.d(n,"pageQuery",function(){return e});t(0);var o=t(227),e="3433664790";n.default=function(n){var t=n.data,e=n.location;return a.createElement(o.a,{blogType:"vocabulary",data:t,location:e})}}.call(this,t(58))}}]);
+//# sourceMappingURL=component---src-pages-vocabulary-all-html-js-c7bd9c3871d01f2a0eef.js.map
